23860d993ca1412df399e71f250c3489c91cf51c
Jaroslaw Nowosad <yarenty@gmail.com>
2020-11-17 10:58:41 +0000

    Initial Commit

---
+++ CUDA_examples/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CUDA_examples
+//
+//  Created by Jaroslaw Nowosad on 17/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
